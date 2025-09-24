142f84f2e5612426c5b6abf9748003d04b632d6b
James Ordinola <james.ordinola@navix.io>
2025-09-24 11:16:46 -0500

    Middlewares 2, regarding organization and how to apply them to some routes

--- express/9.middleware.js
+++ express/9.middleware.js
@@ -2,18 +2,31 @@ const express = require('express')
 
 const app = express()
 
+// Order of the middlewares matters as they will be applied to all 
+// subsequent routes
+
+// Logger middleware
 app.use((req, res, next) => {
-console.log(`Route: ${req.url} Method: ${req.method}`)
+    console.log(`Route: ${req.url} Method: ${req.method}`)
     console.log('went through here')
     next()
 })
 
-app.get('/profile', (req, res) => {
-    res.send('Profile')
-})
-
 app.all('/about', (req, res) => {
     res.send('about')
+})
+
+// isAuthenticated middleware
+app.use((req, res, next) => {
+    if (req.query.login === 'fazt@faztweb.com') {
+        next()
+    } else {
+        res.send('not authorized')
+    }
+})
+
+app.get('/profile', (req, res) => {
+    res.send('Profile')
 })
 
 app.listen(3000)
